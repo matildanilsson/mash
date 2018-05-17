14bc8fecd92c2f9cd6a68b5e041dba088472857b
Hanna Andersson <hannaandersson010@gmail.com>
2018-05-17 10:45:11 +0200

    Added user account

--- imports/ui/layouts/App.js
+++ imports/ui/layouts/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import {withHistory, Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import {withHistory, Link} from 'react-router-dom';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import AccountsUIWrapper from './AccountsUIWrapper.js';
 
-export const App = () => 
-    <div>
-        <h1>Hello world</h1>
-    </div>
+class App extends Component{ 
+    render(){
+        return (
+            <div className="container">
+                <h1>Hello world</h1>
+                <AccountsUIWrapper />
+            </div>
+        );
+    }    
+}
+    
