8f9814b82b08ff45953b83af814204c89e468b65
Matilda <id14mnn@cs.umu.se>
2018-05-18 15:53:21 +0200

    Comments

--- imports/ui/components/pages/Course.js
+++ imports/ui/components/pages/Course.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {withHistory, Link} from 'react-router-dom';
+import {Comment} from '../Comment';
 
 export const Course = () => 
     <div>
         <Link to="/"><h1>Back</h1></Link>
+        <Comment />
     </div>
