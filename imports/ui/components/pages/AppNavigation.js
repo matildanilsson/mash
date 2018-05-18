3eda0e21bc41e5ffc91e02e5b0144ac37f750b0c
Sofia Hedlund <sofia-1994@hotmail.com>
2018-05-18 15:52:10 +0200

    created a form -AddCourse

--- imports/ui/components/pages/AppNavigation.js
+++ imports/ui/components/pages/AppNavigation.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {withHistory, Link} from 'react-router-dom';
+import '../../../api/courses.js';
 
 export const AppNavigation = () => 
     <div>
         <Link to="/Course"> <div className="courseBlock"> </div></Link>
         <Link to="/AddCourse"> <h1> Lägg till kurs </h1></Link>
+        
     </div>
